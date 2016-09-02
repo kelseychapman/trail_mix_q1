f073fd9ed1ab79b1c168a977827623899d699fef
Kelsey Chapman <kelseyc.chapman@gmail.com>
2016-09-02 10:22:40 -0600

    linted html

--- app.js
+++ app.js
@@ -1,47 +1,20 @@
-// Application Keys
-// Below is a list of your current API keys:
-//
-// Application Name: Student Project
-// Key: 0f90d76e419404ba312ccb741f3d0b1f
-// Secret: f8e91df9dae6e461
-// Platform: website/app
-// Status: approved
-// Description: Outdoor activity trail map search
-
-
-
-
 $(document).ready(function() {
 
-    //dropdown functionality
-    // $('.mdb-select').material_select();
-    //end dropdown
-
-    var xhr = new XMLHttpRequest();
-
+var xhr = new XMLHttpRequest();
 
 
     var allJSON = {};
     $('#submit').on('click', function(event) {
         event.preventDefault()
         var activity = $('#activityInput option:selected').val();
-
         var location = $('#locationInput').val();
-        // console.log(city);
-
         let locationParts = location.split(",")
         let city = $.trim(locationParts[0])
         let state = $.trim(locationParts[1])
-            // console.log(city, state);
 
         $('#trails').empty();
-
         getData(activity, city, state)
-
-
     })
-
-
 
     var getData = function(activity, city, state) {
         xhr.open('GET',
@@ -50,29 +23,20 @@ $(document).ready(function() {
         xhr.responeType = 'json'
         xhr.onload = function() {
             if (xhr.status === 200) {
-                // console.log('User\'s name is ' + xhr.responseText);
                 var myJSON = JSON.parse(xhr.responseText)
-                    // console.log('myJSON', myJSON);
-                    // for(key in myJSON){
-                    //   allJSON[key] = myJSON[key];
-                    // }
                 getActivity(myJSON);
             } else {
                 alert('Request failed.  Returned status of ' + xhr.status);
             }
         };
         xhr.send();
-
     }
-
-
 
 
     function getActivity(myJSON) {
         var activitiesArray = []
         console.log(myJSON);
         for (var i = 0; i < myJSON.places.length; i++) {
-            // console.log(myJSON.places[i]);
 
 
             var activityValue = {
@@ -84,10 +48,8 @@ $(document).ready(function() {
                 thumbnail: myJSON.places[i].activities[0].thumbnail,
                 url: myJSON.places[i].activities[0].url,
                 directions: myJSON.places[i].directions
-
             }
             activitiesArray.push(activityValue)
-                // console.log(activityValue)
         }
         console.log(activitiesArray);
         renderActivities(activitiesArray)
@@ -112,15 +74,14 @@ $(document).ready(function() {
             $text.text(activitiesArray[i].description);
             $location.text(activitiesArray[i].city + ' , ' + activitiesArray[i].state);
             $thumbnail.html($img);
-            // console.log($($thumbnail));
 
-              $card.append($cardBlock)
+
+            $card.append($cardBlock)
             $cardBlock.append($thumbnail)
             $cardBlock.append($title)
             $cardBlock.append($activityType)
             $cardBlock.append($location)
             $cardBlock.append($text)
-            // $cardBlock.append($button)
             $col.append($card)
             $("#trails").append($col)
 
@@ -129,12 +90,9 @@ $(document).ready(function() {
             var $button = $('<a href="#" target="blank" class="btn btn-default">')
 
             $button.text('View More Details');
-            $button.attr('href',activitiesArray[i].url);
+            $button.attr('href', activitiesArray[i].url);
             $action.append($button);
             $card.append($action);
         }
-
     }
-
-
 })
